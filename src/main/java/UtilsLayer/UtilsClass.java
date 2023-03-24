fbefc9f5faafce0cc3205cf7c7bf2c67480da143
Suraj Patil <prafpawar11@gmail.com>
2023-03-24 19:19:22 +0530

    BDD Framework is created

---
+++ src/main/java/UtilsLayer/UtilsClass.java
@@ -0,0 +1,11 @@
+package UtilsLayer;
+
+public class UtilsClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("All Selenium Scripts");
+
+	}
+
+}
---
+++ src/test/java/StepDefLayer/LoginPageStepDef.java
@@ -0,0 +1,10 @@
+package StepDefLayer;
+
+public class LoginPageStepDef {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Login Page Step def code");
+	}
+
+}
